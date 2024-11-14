0e62a64a24d68ed023fc4c25a6cf8cc2b3287318
DESKTOP-OSFKUPN\hi <rhksgns4@gmail.com>
2024-11-14 10:52:53 +0900

    <24.11.14 SPRINGBOOT>

---
+++ chap03-view-resolver-lecture-source/src/main/java/com/ohgiraffers/viewresolver/ResovlerController.java
@@ -0,0 +1,63 @@
+package com.ohgiraffers.viewresolver;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class ResovlerController {
+
+    /* comment.
+     *   handler 메소드에서 마지막으로 해야 할 일은
+     *   client 에게 응답해야 할 페이지를 return 하는 것이다.
+     *   스프링에서는 다양한 전략에 맞게 view 를 해석할 수 있는
+     *   ViewResolver 구현체가 존재한다.
+     *   우리가 Thymeleaf 의존성을 추가하게 된다면
+     *   ViewResolver Thymeleaf 문법을 해석할 수 있는
+     *   ThymeleafViewResolver 로 전환이 되며
+     *   이는 prefix 로 resource/templates/suffix 로
+     *   .html 을 자동으로 붙여주게 된다. */
+
+    @GetMapping("string")
+    public String stringReturn(Model model) {
+
+        model.addAttribute("forwardMessage","문자열로 view 반환");
+
+        return "result";
+    }
+
+    /* comment.
+     *   우리가 지금까지 view 를 반환하는 방식은
+     *   default 로 forward 방식이다.
+     *   redirect 를 하는 방법은
+     *   접두사로 redirect: 를 붙이면 된다. */
+    @GetMapping("string-redirect")
+    public String stringRedirect() {
+
+
+
+        return "redirect:/";
+    }
+
+    /* comment.
+    *   redirect 란? 최초 요청시 받게 되면 응답을 진행 후
+    *   다시 재요청을 하는 방식이다.
+    *   따라서 request 가 2번 일어나기 때문에 값을 유지 할 방법이 없다.
+    *   그래서 Servlet 에서 Session, Cookie 라는 개념을 했었다.
+    *   Session 과 Cookie 는 별도의 공간을 할당하기 때문에
+    *   관리 할 데이터가 많아지면 서버의 부담이 생길 수 있다.
+    *   스프링 에서는 RedirectAttribute 라는 타입을 통해
+    *   redirect 시 값을 저장할 수 있는 기능을 제공해준다. */
+    @GetMapping("string-redirect-attribute")
+    public String redirectAttribute(RedirectAttributes redirectAttributes) {
+        /* comment.
+        *   RedirectAttribute 는 Session 의 기능을 사용하지만,
+        *   Flash -> 잠깐 값이 담겼다가 소멸되는 방식을 사용한다.
+        *   Session 처럼 소멸 될 때까지 공간을 차지하는 것이 아닌
+        *   잠시 데이터를 썻다가 사라지는 구조이기 때문에 서버의 부담이 없다. */
+        redirectAttributes.addFlashAttribute("flashMessage","리다이렉트 시 값 유지!!");
+
+        return "redirect:/";
+    }
+}
