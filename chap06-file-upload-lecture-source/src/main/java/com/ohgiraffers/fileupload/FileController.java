0ebc130dc253f61ee02b8f31e82d7483fb056b2c
DESKTOP-OSFKUPN\hi <rhksgns4@gmail.com>
2024-11-18 13:05:47 +0900

    <24.11.18 SPRINGBOOT>

--- chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileController.java
+++ chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -79,5 +80,11 @@ public class FileController {
 
         return "result";
 
+    }
+
+    @PostMapping("multi-file")
+    public String multiFile(@RequestParam List<MultipartFile> multiFile, String description, Model model) {
+
+        return "result";
     }
 }
