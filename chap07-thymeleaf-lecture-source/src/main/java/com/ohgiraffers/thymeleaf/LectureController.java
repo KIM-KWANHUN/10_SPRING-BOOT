424b7f1b9c2ba694545d610b3a955d32b62e105c
DESKTOP-OSFKUPN\hi <rhksgns4@gmail.com>
2024-11-19 11:37:33 +0900

    <24.11.19 SPRINGBOOT>

--- chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/LectureController.java
+++ chap07-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/LectureController.java
@@ -15,10 +15,16 @@ public class LectureController {
     public ModelAndView expression(ModelAndView mv) {
 
         // 요청받은 것을 반환 해 줄 뷰에 대한 경로(우리가 지정 한 이름으로 templates 에 .html 로 만들어야 함)
+
+        mv.addObject("member", new MemberDTO("김관훈",30,'남',"경기도 파주시"));
+        // memberDTO 파일 가져와서 값 넣어주기 KEY 값 = member VALUE 값 = new MemberDTO
         mv.setViewName("lecture/expression");
+
+        mv.addObject("hello","hi~<h2>타임리프</h2>");
 
         return mv;
     }
+
 
 
 }
